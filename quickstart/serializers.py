94c5d08272068ca4abe8d5099f0875055066c226
WesleyMagi <wesley.m93@gmail.com>
2018-04-10 09:47:06 +0200

    Intial Commit: Project set-up and configuration.

---
+++ quickstart/serializers.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField(
+        max_length=100,
+        style={'placeholder': 'Email', 'autofocus': True}
+    )
+    password = serializers.CharField(
+        max_length=100,
+        style={'input_type': 'password', 'placeholder': 'Password'}
+    )
+    remember_me = serializers.BooleanField()
