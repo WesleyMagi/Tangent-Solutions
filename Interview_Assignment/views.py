567c20f4b97d124265a15e9522e75344f649475b
WesleyMagi <wesley.m93@gmail.com>
2018-04-11 18:43:22 +0200

    Sorted out non_field_errors error.

--- Interview_Assignment/views.py
+++ Interview_Assignment/views.py
@@ -17,17 +17,15 @@ class LoginView(APIView):
      payload = {'username':request.data['username'], 'password':request.data['password']}
      r = requests.post(url, payload)
      token = r.json()
-        
      if(token['token']):
       return Response(token)
 
 class WhoAmI(APIView):
-    def get(self, request, format=None):
-     url = 'http://staging.tangent.tngnt.co/api-token-auth/'
-     payload = {'username':request.data['username'], 'password':request.data['password']}
-     r = requests.post(url, payload)
-     token = r.json()   
-     #url = 'http://staging.tangent.tngnt.co/api/employee/me/'
-     #r = requests.get(url,  headers={'Authorization': 'Token {}'.format(token['token'])})
-     if(token['token']):
-      return Response(r.json())
+    def get(self, request):
+     url = 'http://staging.tangent.tngnt.co/api/employee/me/'
+     payload = {'Authorization':request.META['Authorization']}
+     #head = request.dta
+     r = requests.get(url, headers = payload)
+     token = r.json()
+     #r = requests.get(url,  headers={'Authorization': 'Token {}'.format(token['token'])})     
+     return Response(token)
